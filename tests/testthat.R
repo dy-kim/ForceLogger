f8b8c8a028b02961173de169b33388988c08ee70
jeffry82 <jeffry82@encoredtech.com>
2016-06-22 20:18:13 +0900

    Added the logging module (#1)

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(ForceLogger)
+
+test_check("ForceLogger")
---
+++ tests/testthat/test.R
@@ -0,0 +1,17 @@
+context("logging of ForceLogger")
+
+test_that("learn)", {
+  FORCE_SET_LOGGING(threshold = "TRACE", logging = TRUE)
+  FORCE_TRACE("%s test", "FORCE_TRACE")
+  FORCE_DEBUG("%s test", "FORCE_DEBUG")
+  FORCE_INFO("%s test", "FORCE_INFO")
+  FORCE_WARN("%s test", "FORCE_WARN")
+  FORCE_ERROR("%s test", "FORCE_ERROR")
+  tryCatch(
+    expr = FORCE_FATAL("%s test", "FORCE_FATAL"),
+    error = function(e) {
+      print(sprintf("Catched: %s", e$message))
+    }
+  )
+  print(FORCE_GET_LOG())
+})
