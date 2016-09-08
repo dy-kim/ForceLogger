5e911573f2d65055b529a62447c34c519141b1a1
Daeyoung Kim <daykim22@gmail.com>
2016-09-08 17:11:30 +0900

    (작은) 리팩토링  (#4)
    * Use roxygen2 to generate NAMESPACE. Replace long if-else statement to switch statement.
    
    * Check function input arg by adding match.arg() line

--- R/log.R
+++ R/log.R
@@ -1,62 +1,69 @@
-#threshold : "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL"
-FORCE_SET_LOGGING <- function(threshold = "INFO",
-                              logging = FALSE) {
-  if (threshold == "TRACE")
-    threshold_type <- TRACE
-  else if (threshold == "DEBUG")
-    threshold_type <- DEBUG
-  else if (threshold == "INFO")
-    threshold_type <- INFO
-  else if (threshold == "WARN")
-    threshold_type <- WARN
-  else if (threshold == "ERROR")
-    threshold_type <- ERROR
-  else if (threshold == "FATAL")
-    threshold_type <- FATAL
-  else
-    threshold_type <- INFO
-  
-  assign("force_log_logging_", logging, envir = .GlobalEnv)
-  flog.appender(force_log_handler(), name = "force")
-  flog.threshold(threshold_type, name = "force")
-  layout <- layout.format('[~l][~t][~n.~f] ~m')
-  flog.layout(layout, name = "force")
-}
+#' @export
+FORCE_SET_LOGGING <-
+  function(threshold = c("INFO", "TRACE", "DEBUG", "WARN", "ERROR", "FATAL"),
+           logging = FALSE) {
+    if (missing(threshold))
+      threshold_type <- "INFO"
 
-force_log_handler <- function()
-{
+    threshold <- match.arg(threshold)
+    threshold_type <-
+      switch(
+        EXPR = threshold,
+        TRACE = TRACE,
+        DEBUG = DEBUG,
+        INFO = INFO,
+        WARN = WARN,
+        ERROR = ERROR,
+        FATAL = FATAL
+      )
+
+    assign("force_log_logging_", logging, envir = .GlobalEnv)
+    flog.appender(force_log_handler(), name = "force")
+    flog.threshold(threshold_type, name = "force")
+    layout <- layout.format("[~l][~t][~n.~f] ~m")
+    flog.layout(layout, name = "force")
+  }
+
+#' @export
+force_log_handler <- function() {
   function(line) {
     if (force_log_logging_) {
-      cat(line, sep = '', file = stderr())
+      cat(line, sep = "", file = stderr())
     }
   }
 }
 
+#' @export
 FORCE_TRACE <- function(fmt, ...) {
   msg <- sprintf(fmt, ...)
   flog.trace(msg, name = "force")
 }
 
+#' @export
 FORCE_DEBUG <- function(fmt, ...) {
   msg <- sprintf(fmt, ...)
   flog.debug(msg, name = "force")
 }
 
+#' @export
 FORCE_INFO <- function(fmt, ...) {
   msg <- sprintf(fmt, ...)
   flog.info(msg, name = "force")
 }
 
+#' @export
 FORCE_WARN <- function(fmt, ...) {
   msg <- sprintf(fmt, ...)
   flog.warn(msg, name = "force")
 }
 
+#' @export
 FORCE_ERROR <- function(fmt, ...) {
   msg <- sprintf(fmt, ...)
   flog.error(msg, name = "force")
 }
 
+#' @export
 FORCE_FATAL <- function(fmt, ...) {
   msg <- sprintf(fmt, ...)
   flog.fatal(msg, name = "force")
---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+#' @import futile.logger
+NULL
+
+.onAttach <- function(libname, pkgname) {
+  ver <- read.dcf(file = system.file("DESCRIPTION", package = pkgname),
+                  fields = "Version")
+  packageStartupMessage("\nPackage ", pkgname, " (", ver, ") loaded.")
+}
